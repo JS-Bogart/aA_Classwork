cf1d0f3fc6427a8f0bedd3d8a881a4a13cffcc8d
caroger <rogerhu1989@gmail.com>
2020-12-11 15:26:39 -0800

    finished implemented myQueue

--- W4D5/max_window_range.rb
+++ W4D5/max_window_range.rb
@@ -11,14 +11,35 @@ def windowed_max_range(arr, w)
   current_max_range
 end
 
-p windowed_max_range([1, 0, 2, 5, 4, 8], 2) # == 4 # 4, 8
-p windowed_max_range([1, 0, 2, 5, 4, 8], 3) #== 5 # 0, 2, 5
-p windowed_max_range([1, 0, 2, 5, 4, 8], 4) # == 6 # 2, 5, 4, 8
-p windowed_max_range([1, 3, 2, 5, 4, 8], 5)  #== 6 # 3, 2, 5, 4, 8
+# p windowed_max_range([1, 0, 2, 5, 4, 8], 2) # == 4 # 4, 8
+# p windowed_max_range([1, 0, 2, 5, 4, 8], 3) #== 5 # 0, 2, 5
+# p windowed_max_range([1, 0, 2, 5, 4, 8], 4) # == 6 # 2, 5, 4, 8
+# p windowed_max_range([1, 3, 2, 5, 4, 8], 5)  #== 6 # 3, 2, 5, 4, 8
 
 
 class MyQueue
+
   def initialize
     @store = []
+  end
+
+  def peek
+    @store.first
+  end
+
+  def size
+    @store.length
+  end
+
+  def empty?
+    @store.nil?
+  end
+
+  def enqueue(ele)
+    @store.push(ele)
+  end
+
+  def dequeue
+    @store.shift
   end
 end
