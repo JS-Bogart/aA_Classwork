4e32c0f8c1f9555e2708ac398074e22b64886205
Bogey <jeffreysbogart@gmail.com>
2020-12-16 14:51:58 -0800

    Adds user class

---
+++ W5D3/aa_questions/user.rb
@@ -0,0 +1,25 @@
+require 'sqlite3'
+require_relative 'questions_db.rb'
+
+class User
+    attr_accessor :fname, :lname
+    
+    def initialize(hash)
+        @id = hash['id']
+        @fname = hash['fname']
+        @lname = hash['lname']
+    end
+
+    def self.find_by_id(id)
+        response = QuestionsDatabase.instance.execute(<<-SQL, id)
+            SELECT
+                *
+            FROM
+                users
+            WHERE
+                id = ?
+        SQL
+        User.new(response[0])
+    end
+
+end
