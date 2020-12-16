c3ed58cdbbb8d23c876253d4146503f3423fc000
Bogey <jeffreysbogart@gmail.com>
2020-12-16 14:09:46 -0800

    Adds tables to import_db

---
+++ W5D3/aa_questions/import_db.sql
@@ -0,0 +1,31 @@
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    fname TEXT NOT NULL,
+    lname TEXT NOT NULL
+);
+
+CREATE TABLE questions (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    author_id INTEGER NOT NULL,
+    FOREIGN KEY (author_id) references users(id)
+);
+
+CREATE TABLE question_follows (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) references users(id),
+    FOREIGN KEY (question_id) references questions(id)
+);
+
+CREATE TABLE replies (
+    id INTEGER PRIMARY KEY,
+    parent_id INTEGER,
+    author_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+    body TEXT NOT NULL
+)
