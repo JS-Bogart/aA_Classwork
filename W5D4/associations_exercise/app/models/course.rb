ebb75917458592d875e19c6be3099ecdfeb81722
shinara03 <shinara03@gmail.com>
2020-12-17 15:11:22 -0800

    Changes to user.rb, course.rb, and enrollment.rb

--- W5D4/associations_exercise/app/models/course.rb
+++ W5D4/associations_exercise/app/models/course.rb
@@ -15,4 +15,18 @@ class Course < ApplicationRecord
     foreign_key: :course_id,
     class_name: :Enrollment
 
+  has_many :pre_requisites,
+    primary_key: :id,
+    foreign_key: :prereq_id,
+    class_name: :Course
+
+  belongs_to :instructor,
+    primary_key: :id,
+    foreign_key: :instructor_id,
+    class_name: :User
+
+  has_many :enrolled_students,
+    through: :enrollments,
+    source: :user
+
 end
--- W5D4/associations_exercise/app/models/user.rb
+++ W5D4/associations_exercise/app/models/user.rb
@@ -13,7 +13,12 @@ class User < ApplicationRecord
     foreign_key: :student_id,
     class_name: :Enrollment
 
-    has_many :enrolled_courses,
-      through: :course,
-      source: :Course
+  # has_many :taught_courses,
+  #   primary_key: :id,
+  #   foreign_key: :instructor_id,
+  #   class_name: :Course
+
+  has_many :enrolled_courses,
+    through: :enrollments,
+    source: :course
 end
