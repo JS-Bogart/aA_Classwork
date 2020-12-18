a7ffb4207f80a2c4c1a73a3bf83d8d96b7d51f73
shinara03 <shinara03@gmail.com>
2020-12-17 17:00:36 -0800

    changes to shortenedUrl class

--- W5D4/URLShortener/app/models/shortenedurl.rb
+++ W5D4/URLShortener/app/models/shortenedurl.rb
@@ -1,4 +1,17 @@
 class ShortenedUrl < ApplicationRecord
   validates :short_url, presence: true, uniqueness: true
   validates :long_url, presence: true, uniqueness: true
+
+  def self.random_code
+    short_url = ""
+    new_url = false
+
+    while !new_url 
+      new_url = true
+      short_url = SecureRandom.urlsafe_base64
+      new_url = false if ShortenedUrl.exists?(short_url)
+    end
+    short_url
+  end
+  
 end
