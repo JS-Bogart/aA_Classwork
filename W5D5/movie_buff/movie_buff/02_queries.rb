300096bcf62617e518f6c9ca2943b157d80ab1ef
Bogey <jeffreysbogart@gmail.com>
2020-12-18 15:26:36 -0800

    Finishes second queries

--- W5D5/movie_buff/movie_buff/02_queries.rb
+++ W5D5/movie_buff/movie_buff/02_queries.rb
@@ -19,7 +19,11 @@ end
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
-  
+  Actor
+    .select(:id, :name)
+    .joins(:movies)
+    .where(movies: {title: title})
+    .order('castings.ord')
 end
 
 def vanity_projects
@@ -28,11 +32,21 @@ def vanity_projects
   # Show the movie id and title and director's name.
 
   # Note: Directors appear in the 'actors' table.
-
+  Movie
+    .select(:id, :title, 'actors.name')
+    .joins(:actors)
+    .where('director_id = actors.id')
+    .where(castings: {ord: 1})
 end
 
 def most_supportive
   # Find the two actors with the largest number of non-starring roles.
   # Show each actor's id, name and number of supporting roles.
-
+  Actor
+    .select(:id, :name, 'COUNT(movies.id) as roles')
+    .joins(:movies)
+    .group('actors.id')
+    .where.not(castings: {ord: 1})
+    .order('roles DESC')
+    .limit(2)
 end
