f5b21d564cdc357210271419dcb29747ca68381a
Bogey <jeffreysbogart@gmail.com>
2020-12-17 16:48:45 -0800

    Adds models for URL shortener

--- W5D4/URLShortener/db/schema.rb
+++ W5D4/URLShortener/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_18_002400) do
+ActiveRecord::Schema.define(version: 2020_12_18_004647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_12_18_002400) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["long_url"], name: "index_shortened_urls_on_long_url", unique: true
+    t.index ["short_url"], name: "index_shortened_urls_on_short_url", unique: true
   end
 
   create_table "users", force: :cascade do |t|
