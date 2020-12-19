1d2d9b6e90f1206975c08e6b43ff3abfbe03c9c6
Bogey <jeffreysbogart@gmail.com>
2020-12-18 17:03:45 -0800

    Creates several migrations

--- W5D5/polls_app/db/migrate/20201219001942_create_users.rb
+++ W5D5/polls_app/db/migrate/20201219001942_create_users.rb
@@ -1,9 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :, null: false
+      t.string :username, null: false
 
       t.timestamps
     end
+    add_index :users, :username, unique: true
   end
 end
