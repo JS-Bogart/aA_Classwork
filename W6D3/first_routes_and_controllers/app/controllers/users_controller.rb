65c053f09522851b803f7e41c3f3bc35f23db9c9
Bogey <jeffreysbogart@gmail.com>
2020-12-29 15:31:11 -0800

    Adds update, show, and destroy methods to user controller, and completes project

--- W6D3/first_routes_and_controllers/app/controllers/users_controller.rb
+++ W6D3/first_routes_and_controllers/app/controllers/users_controller.rb
@@ -1,15 +1,46 @@
 class UsersController < ApplicationController
     
     def index
-        render plain: "I'm in the index action!"
+        render json: User.all
     end
 
     def create
-        render json: params
+        user = User.new(user_params)
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def show
-        render json: params
+        user = User.find_by(id: params[:id])
+
+        render json: user
+    end
+
+    def update
+        user = User.find_by(id: params[:id])
+
+        if user.update(user_params)
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        user = User.find_by(id: params[:id])
+
+        if user.destroy
+            render json: user
+        else
+            render json: "Cannot destroy this user."
+        end
+    end
+
+    def user_params
+        params.require(:user).permit(:name, :email)
     end
 
 end
