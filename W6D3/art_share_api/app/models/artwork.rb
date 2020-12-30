330d54dfa35c0866454696acba687a118901f8c1
Jadin <jadinp@gmail.com>
2020-12-30 14:31:34 -0800

    Add User, Artwork, and ArtworkShare models

--- W6D3/art_share_api/app/models/artwork.rb
+++ W6D3/art_share_api/app/models/artwork.rb
@@ -1,5 +1,22 @@
 class Artwork < ApplicationRecord
     validates :title, :artist_id, :image_url, presence: true
-    validates "#{:title}#{:artist_id}", uniqueness: true
-    validates :image_url, uniqueness: true
+    validates :image_url, :artist_id, uniqueness: true
+    validates :title, uniqueness: { scope: :artist_id }
+
+    belongs_to :artist,
+        class_name: :User,
+        foreign_key: :artist_id,
+        primary_key: :id
+
+    has_many :artwork_shares
+        class_name: :ArtworkShare,
+        foreign_key: :artwork_id,
+        primary_key: :id
+
+    has_many :shared_viewers,
+        through: :artwork_shares,
+        source: :viewer
+        
+    
+
 end
--- W6D3/art_share_api/app/models/user.rb
+++ W6D3/art_share_api/app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
+
+    has_many :artworks,
+        class_name: :Artwork,
+        foreign_key: :artist_id,
+        primary_key: :id
+
+    has_many :viewings,
+        class_name: :ArtworkShare,
+        foreign_key: :viewer_id,
+        primary_key: :id
+        
+    has_many :shared_artworks,
+        through: :viewings,
+        source: :artwork
+    
+
 end
