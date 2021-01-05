124be3c3a00af5825cf8b73f62e418a686e7a05a
Bogey <jeffreysbogart@gmail.com>
2021-01-05 11:47:50 -0800

    Adds user view files

--- W7D2/music_app/app/controllers/users_controller.rb
+++ W7D2/music_app/app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save
-            redirect_to user_url(@user)
+            redirect_to users_url
         else
             render :new
         end
--- W7D2/music_app/config/routes.rb
+++ W7D2/music_app/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:new, :create] # mb we will need :show also
+  resources :users, only: [:new, :create, :index] # mb we will need :show also
+  resource :session, only: [:new, :create, :destroy]
 
 end
