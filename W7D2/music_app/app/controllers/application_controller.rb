67af5ca0502b99c8e315cfe1e322783dd62768a1
Dmitrii An <an.dmitriy93@gmail.com>
2021-01-06 02:13:02 +0600

    SessionsController  working on destroy

--- W7D2/music_app/app/controllers/application_controller.rb
+++ W7D2/music_app/app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
     skip_before_action :verify_authenticity_token #delete later
+
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def login_user!(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
 end
--- W7D2/music_app/app/controllers/sessions_controller.rb
+++ W7D2/music_app/app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+    helper_method :current_user
+
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def create
+        @user = User.find_by_credentials(
+            params[:user][:email],
+            params[:user][:password]
+        )
+
+        @user.reset_session_token!
+        redirect_to user_url(@user)
+    end
+
+    def destroy
+        session = 
+    end
+
 end
