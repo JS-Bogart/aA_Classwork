0aa29b93ec445b6eba50b8a8a04eeefe54ffd697
Bogey <jeffreysbogart@gmail.com>
2021-01-04 17:19:07 -0800

    Adds 99cats II to my repo

---
+++ W7D1/ninety_nine_cats_ii/app/models/user.rb
@@ -0,0 +1,33 @@
+class User < ApplicationRecord
+
+    attr_reader :password
+
+    validates :user_name, :password_digest, :session_token, presence: true
+    validates :password, length: { minimum: 6 }, allow_nil: true 
+
+
+    after_initialize :reset_session_token!
+
+    def reset_session_token!
+        self.session_token ||= SecureRandom::urlsafe_base64
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def self.find_by_credentials(user_name, password)
+        user = User.find_by(user_name: user_name)
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+end
