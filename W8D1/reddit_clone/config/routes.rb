af71800919922444adf71e8d793f4b40f275fbc4
Bogey <jeffreysbogart@gmail.com>
2021-01-11 12:03:13 -0800

    Adds posts model

--- W8D1/reddit_clone/config/routes.rb
+++ W8D1/reddit_clone/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :show, :index, :create] do 
-    resources :subs, only: [:edit, :update]
+    resources :subs, only: [:edit]
   end
   resource :session, only: [:new, :create, :destroy]
-  resources :subs, only: [:new, :index, :show, :create]
+  resources :subs, only: [:new, :index, :show, :create, :update]
 end
--- W8D1/reddit_clone/db/schema.rb
+++ W8D1/reddit_clone/db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_11_184951) do
+ActiveRecord::Schema.define(version: 2021_01_11_195630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "url"
+    t.text "content"
+    t.integer "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "sub_id", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id", unique: true
+    t.index ["sub_id"], name: "index_posts_on_sub_id", unique: true
+    t.index ["title"], name: "index_posts_on_title", unique: true
+  end
 
   create_table "subs", force: :cascade do |t|
     t.string "title", null: false
