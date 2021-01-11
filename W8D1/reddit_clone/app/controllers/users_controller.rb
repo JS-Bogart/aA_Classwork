e5d777aa4f2e510b0f06d516ddd4677a4de24a4f
Bogey <jeffreysbogart@gmail.com>
2021-01-11 09:32:56 -0800

    Adds user controller and model

---
+++ W8D1/reddit_clone/app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            sign_in(@user)
+            redirect_to users_url
+        else
+            flash.now[:errors] = @users.errors.full_messages
+            render :new
+        end
+    end
+
+    def show
+        render :show
+    end
+
+    def index
+        @users = User.all 
+        render :index
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+
+end
