c839ce250c3ef86054b29512b328d59e3a29901d
Bogey <jeffreysbogart@gmail.com>
2021-01-12 14:22:00 -0800

    Completes basic_server

--- W8D2/rails_lite/bin/p01_basic_server.rb
+++ W8D2/rails_lite/bin/p01_basic_server.rb
@@ -1,1 +1,22 @@
 require 'rack'
+
+# app = Proc.new do |env|
+#   req = Rack::Request.new(env)
+#   res = Rack::Response.new
+#   res['Content-Type'] = 'text/html'
+#   res.write("Hello world!")
+#   res.finish
+# end
+
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  res['Content-Type'] = 'text/html'
+  res.write("#{env["REQUEST_PATH"]}")
+  res.finish
+end
+
+Rack::Server.start(
+  app: app,
+  Port: 3000
+)
