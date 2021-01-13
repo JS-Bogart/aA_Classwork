41a7cfa10c2004e972d3aad8204fe844d1f2ecf9
Bogey <jeffreysbogart@gmail.com>
2021-01-12 17:17:08 -0800

    Adds initialize to session.rb

--- W8D2/rails_lite/lib/controller_base.rb
+++ W8D2/rails_lite/lib/controller_base.rb
@@ -1,5 +1,6 @@
 require 'active_support'
 require 'active_support/core_ext'
+require 'active_support/inflector'
 require 'erb'
 require_relative './session'
 
@@ -8,25 +9,40 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    self.res.header['location'] = url
+    self.res.status = 302
+    raise if @already_built_response
+    @already_built_response = true
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    res['Content-Type'] = content_type
+    res.write(content)
+    raise if @already_built_response
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    path = "views/#{self.class.name.underscore}/#{template_name}.html.erb"
+    template = ERB.new(File.read(path))
+    content = template.result(binding)
+    render_content(content, 'text/html')
   end
 
   # method exposing a `Session` object
--- W8D2/rails_lite/lib/session.rb
+++ W8D2/rails_lite/lib/session.rb
@@ -1,12 +1,19 @@
 require 'json'
+require 'byebug'
 
 class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+    if req.cookies['_rails_lite_app']
+      @cookie_value = JSON.parse(req.cookies['_rails_lite_app'])
+    else
+      @cookie_value = {}
+    end
   end
 
   def [](key)
+    @cookie_value[key]
   end
 
   def []=(key, val)
