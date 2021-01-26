0f85ac86905f16b1e4fb5c17143a85045bd5dc51
Bogey <jeffreysbogart@gmail.com>
2021-01-26 15:41:04 -0800

    Add css styling

--- W10D2/widgets/frontend/clock.jsx
+++ W10D2/widgets/frontend/clock.jsx
@@ -23,10 +23,19 @@ class Clock extends React.Component {
 
     render() {
         return (
-            <>
-                <h1>Current Time</h1>
-                <h2>{this.state.time.getHours()} : {this.state.time.getMinutes()} : {this.state.time.getSeconds()}</h2>
-            </>
+            <div className="clock-widget">
+                <h1>Clock</h1>
+                <div className="full-clock">
+                    <div className="time">
+                        <h2>Time</h2>
+                        <h3>{this.state.time.getHours()} : {this.state.time.getMinutes()} : {this.state.time.getSeconds()}</h3>
+                    </div>
+                    <div className="date">
+                        <h2>Date</h2>
+                        <h3>{this.state.time.getMonth() + 1}/{this.state.time.getDate()}/{this.state.time.getFullYear()}</h3>
+                    </div>
+                </div>
+            </div>
         )
     }
 
