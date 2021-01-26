f686187cf059ff72b3ebec1900586efaf132a227
Michelle Ha <michelle1992us@yahoo.com>
2021-01-26 14:38:36 -0800

    starting on rendering clock widget

--- W10D2/widgets/frontend/clock.jsx
+++ W10D2/widgets/frontend/clock.jsx
@@ -1,5 +1,36 @@
 import React from 'react';
 
-const Clock = () => <h1>Congratulations, you did it!</h1>;
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = new Date()
+    }
+
+    componentDidMount() {
+       let time = setInterval(tick, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(time);
+    }
+
+    tick() {
+        this.setState(new Date())
+    }
+
+    render() {
+        return (
+            <>
+                <h1>Clock</h1>
+                
+            </>
+        )
+    }
+
+
+
+}
+
+
 
 export default Clock;
