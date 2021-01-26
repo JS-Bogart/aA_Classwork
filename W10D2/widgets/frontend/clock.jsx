711e7e2c0383bd4708e3f4111f614069efa59687
Bogey <jeffreysbogart@gmail.com>
2021-01-26 14:54:09 -0800

    Remove errors from setInterval

--- W10D2/widgets/frontend/clock.jsx
+++ W10D2/widgets/frontend/clock.jsx
@@ -3,19 +3,23 @@ import React from 'react';
 class Clock extends React.Component {
     constructor(props) {
         super(props);
-        this.state = new Date()
+        this.state = {
+            time: new Date(),
+        }
+        this.tick = this.tick.bind(this);
+        this.interval = setInterval(this.tick, 1000);
     }
 
     componentDidMount() {
-       let time = setInterval(tick, 1000);
+       this.interval = setInterval(this.tick, 1000);
     }
 
     componentWillUnmount() {
-        clearInterval(time);
+        clearInterval(this.interval);
     }
 
     tick() {
-        this.setState(new Date())
+        this.setState({time: new Date()});
     }
 
     render() {
