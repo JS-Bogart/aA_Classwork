289d8e9fd2e25e45797a22cefea0cf34c35fc770
Bogey <jeffreysbogart@gmail.com>
2021-01-27 11:40:04 -0800

    Add game, board, and tile files

---
+++ W10D3/minesweeper/components/game.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import * as Minesweeper from './minesweeper';
+import Board from './board';
+
+class Game extends React.Component {
+    constructor() {
+        const board = new Minesweeper.Board(10, 5);
+        this.state = {
+            board: board
+        }
+        this.updateGame = this.updateGame.bind(this);
+    }
+
+    updateGame(){
+
+    }
+
+    render(){
+        return(
+            <div>
+                <Board board = {this.state.board} updateGame = {this.updateGame}/>
+            </div>
+        )
+    }
+}
+
+export default Game;
---
+++ W10D3/minesweeper/components/tile.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Tile extends React.Component {
+    // constructor(){
+
+    // }
+
+    render() {
+        return(
+            <p>T</p>
+        )
+    }
+}
+
+export default Tile;
